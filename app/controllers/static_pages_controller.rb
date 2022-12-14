6c1ca760d647d0ba8ab4260d5af293ad885454e6
abuaisheh24 <labuaisheh@everconnect.com>
2022-12-14 17:22:07 +0300

    redis simple practice edited

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,14 +3,16 @@ class StaticPagesController < ApplicationController
     require 'redis'
 
     cash = Redis.new(host: 'localhost')
-    email = cash.set('email','laith@gmail.com')
+    cash.set('email','laith@gmail.com')
+    email = cash.get('email')
     #store password 10 second
-    secret_info = cash.setex('password',10,"123456789")
+    cash.setex('password',10,"123456789")
+    secret_info = cash.get('password')
 
     cash.zadd('popular',10,'Laith')
     cash.zadd('popular',8,'Ahmmad')
     cash.zadd('popular',15,'Ali')
 
-    puts "==========",email,"==========",secret_info,"============",cash.zrevrange(0,0),"===========",cash.zrevrange(0,-1)
+    puts "==========",email,"==========",secret_info,"============",cash.zrevrange('popular',0,0),"===========",cash.zrevrange('popular',0,-1)
   end
 end
